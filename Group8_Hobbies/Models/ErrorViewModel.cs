ab705b04da4c710853e40354950553615176f0b4
xiaox2 <xiaox2@VDI-CECH-IC-10>
2022-04-07 14:47:53 -0400

    Add project files.

---
+++ Group8_Hobbies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Group8_Hobbies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
