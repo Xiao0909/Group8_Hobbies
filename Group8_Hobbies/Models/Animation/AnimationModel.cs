848810d5153509b959f5a6c0646543e5114bd620
Xiao Xiao <xiaox2@mail.uc.edu>
2022-04-07 18:11:59 -0400

    create model for animation

---
+++ Group8_Hobbies/Models/Animation/AnimationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group8_Hobbies.Models.Animation
+{
+    public class AnimationModel
+    {
+        public int AnimeId { get; set; }
+        public string Name { get; set; }
+        public int PublishYear { get; set; }
+        public int? WatchingYear { get; set; }
+        public int Rate { get; set; }
+        public int PublisherId { get; set; }
+        public PublishModel Publish { get; set; }
+        
+    }
+}
---
+++ Group8_Hobbies/Models/Animation/AnimeContextModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Group8_Hobbies.Models
+{
+    public class AnimeContextModel : DbContext
+    {
+        public AnimeContextModel(DbContextOptions<AnimeContextModel> options) 
+            : base(options) { }
+        
+        public DbSet<Animation.AnimationModel> Animations { get; set; }
+        public DbSet<Animation.PublishModel> Publishes { get; set; }
+    }
+}
