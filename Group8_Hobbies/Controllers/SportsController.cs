accfac9d584b919da30d1047464113f1b6a2358d
Rahul Shakya <shakyarl@mail.uc.edu>
2022-04-20 01:39:47 -0400

    Add Controller and View

---
+++ Group8_Hobbies/Controllers/SportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group8_Hobbies.Controllers
+{
+    public class SportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
