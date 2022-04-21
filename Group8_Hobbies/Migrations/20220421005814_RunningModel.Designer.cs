cbbe9dab1ae98223b455828f42fe141a6906321a
tracyjn@mail.uc.edu <tracyjn@mail.uc.edu>
2022-04-20 21:05:54 -0400

    Created page for running hobby

--- Group8_Hobbies/Migrations/20220421005814_RunningModel.Designer.cs
+++ Group8_Hobbies/Migrations/20220421005814_RunningModel.Designer.cs
@@ -4,14 +4,16 @@ using Group8_Hobbies.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Group8_Hobbies.Migrations
 {
     [DbContext(typeof(HobbiesContextModel))]
-    partial class HobbiesContextModelModelSnapshot : ModelSnapshot
+    [Migration("20220421005814_RunningModel")]
+    partial class RunningModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
