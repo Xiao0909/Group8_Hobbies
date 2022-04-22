7cf21f05302e6ab7f3f14b64a71148360e3847fa
roycejc <93340291+roycejc@users.noreply.github.com>
2022-04-22 13:19:31 -0400

    Add files via upload

--- Controllers/AnimationController.cs
+++ Controllers/AnimationController.cs
@@ -6,14 +6,76 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Group8_Hobbies.Controllers
 {
     public class AnimationController : Controller
     {
+        private HobbiesContextModel context { get; set; }
+        public AnimationController (HobbiesContextModel ctx)
+        {
+            context = ctx;
+        }
         public IActionResult Index()
         {
-            return View();
+            var animes = context.Animations.Include(p => p.Publish)
+                .OrderBy(n => n.Name).ToList();
+            return View(animes);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new AnimationModel());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var anime = context.Animations.Find(id);
+            return View(anime);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AnimationModel anime)
+        {
+            if (ModelState.IsValid)
+            {
+                if(anime.AnimeId == 0)
+               
+                    context.Animations.Add(anime);
+                
+                else
+                
+                    context.Animations.Update(anime);
+                
+                context.SaveChanges();
+            }
+            else
+            {
+                ViewBag.Action = (anime.AnimeId == 0) ? "Add" : "Edit";
+            }
+            return View(anime);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            ViewBag.Action = "Edit";
+            var anime = context.Animations.Find(id);
+            return View(anime);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(AnimationModel model)
+        {
+            context.Animations.Remove(model);
+            context.SaveChanges();
+            return RedirectToAction("index", "AnimationController");
+
         }
     }
 }
---
+++ Models/Games/GamesModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Group8_Hobbies.Models.Games
+{
+    public class GamesModel
+    {
+        public int GamesModelID { get; set; }
+        public string GameName { get; set; }
+        public string GameGenre { get; set; }
+        public string AvailablePlatform { get; set; }
+        public string GamePublisher { get; set; }
+        public int PersonalRating { get; set; }
+    }
+}
