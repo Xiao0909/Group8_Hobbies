6ba983522bf8927e1cdae45a59c791ade4204382
tracyjn@mail.uc.edu <tracyjn@mail.uc.edu>
2022-04-22 14:09:12 -0400

    Added running hobby page

--- Group8_Hobbies/Models/HobbiesContextModel.cs
+++ Group8_Hobbies/Models/HobbiesContextModel.cs
@@ -1,5 +1,6 @@
 ﻿using Group8_Hobbies.Models.Sports;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Group8_Hobbies.Models
 {
@@ -12,6 +13,7 @@ namespace Group8_Hobbies.Models
         public DbSet<PublishModel> Publishes { get; set; }
 
         public DbSet<SportsModel> Sports { get; set; } 
+        public DbSet<RunningModel> Races { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -19,6 +21,7 @@ namespace Group8_Hobbies.Models
             modelBuilder.Entity<PublishModel>().HasKey(x => x.PublisherId);
 
             modelBuilder.Entity<SportsModel>().HasKey(x => x.SportsModelId);
+            modelBuilder.Entity<RunningModel>().HasKey(x => x.RunningId);
 
             modelBuilder.Entity<AnimationModel>().HasData(
 
@@ -123,6 +126,50 @@ namespace Group8_Hobbies.Models
                 }
 
             );
+
+            // running races info
+            modelBuilder.Entity<RunningModel>().HasData(
+              new RunningModel
+              {
+                  RunningId = 1,
+                  Name = "Cincinnati Flying Pig",
+                  Distance = 26.2,
+                  Date = DateTime.Parse("05/01/2022"),
+                  Website = "https://flyingpigmarathon.com"
+              },
+              new RunningModel
+              {
+                  RunningId = 2,
+                  Name = "Cleveland Turkey Trot",
+                  Distance = 3.1,
+                  Date = DateTime.Parse("11/24/2022"),
+                  Website = "https://www.turkeytrotcleveland.com"
+              },
+              new RunningModel
+              {
+                  RunningId = 3,
+                  Name = "Boston Marathon",
+                  Distance = 26.2,
+                  Date = DateTime.Parse("04/18/2022"),
+                  Website = "https://www.baa.org"
+              },
+              new RunningModel
+              {
+                  RunningId = 4,
+                  Name = "Bolder Boulder 10k",
+                  Distance = 6.2,
+                  Date = DateTime.Parse("05/30/2022"),
+                  Website = "https://www.bolderboulder.com"
+              },
+              new RunningModel
+              {
+                  RunningId = 5,
+                  Name = "Chicago Marathon",
+                  Distance = 26.2,
+                  Date = DateTime.Parse("08/09/2022"),
+                  Website = "https://www.chicagomarathon.com"
+              }
+          );
 
         }
     }
